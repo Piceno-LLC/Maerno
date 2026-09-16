a0d11c6825004d213361bce0a6438adeae7cf389
MAP01 <maguilera131113@gmail.com>
2026-09-16 10:53:19 -0700

    Upgraded

---
+++ generator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @brief Calculates the geometric mean of a dataset.
+ * @note Input vector must not contain negative values.
+ * @param data A vector of doubles representing the source dataset.
+ * @return The calculated geometric mean as a double.
+ * @throws std::invalid_argument If the dataset is empty.
+ */
+class generator {
+public:
+	//static int generate();
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,55 @@
+/*
+
+Copyright Piceno LLC. All rights reserved.
+
+Programmers{
+	Mario Aguilera Piceno - Founder
+}
+
+About: Program that contains multiple software tools, all packed in one.
+
+Created: 09/16/2026
+Last Modified: 09/16/2026
+
+*/
+
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdint>
+
+// Int array to hold initial color data
+struct RGB {
+	uint8_t r, g, b;
+};
+
+// Generates and returns array of ints representing val in an array form
+std::vector<int> toIntVector(int val) {
+	std::vector<int> nums;
+	if (val == 0) {
+		nums.push_back(0);
+	} else {
+		while (val > 0) {
+			nums.push_back(val % 10);
+			val /= 10;
+		}
+		std::reverse(nums.begin(), nums.end());
+	}
+	return nums;
+}
+
+// Converts and returns num vector to char
+std::string toString(std::vector<int> nums) {
+	std::string numsString = "";
+	int ascii_default = 65;
+	for (auto x : nums) {
+		numsString += static_cast<char>(ascii_default + x);
+	}
+	return numsString;
+}
 
 int main() {
-	int ascii_default = 65;
-	int x = 255, y = 0, z = 0;
-
-	std::string strN = "" + static_cast<char>(x) + static_cast<char>(y) + static_cast<char>(z);
+	RGB color(0, 34, 255);
+	std::string strN = "Test String: " + toString((toIntVector(color.r))) + toString((toIntVector(color.g))) + toString((toIntVector(color.b)));
 	std::cout << strN << std::endl;
 }
